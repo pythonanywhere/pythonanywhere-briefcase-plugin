74573d47216ac7001f00a91c55f8002112861c3c
Filip Lajszczak <filip@lajszczak.dev>
2026-02-27 10:18:55 +0000

    bump up version for release.

--- src/pythonanywhere_briefcase_plugin/__init__.py
+++ src/pythonanywhere_briefcase_plugin/__init__.py
@@ -1,4 +1,4 @@
 from pythonanywhere_briefcase_plugin.channel import PythonAnywherePublicationChannel
 
-__version__ = "0.0.1a2"
+__version__ = "0.0.1a3"
 __all__ = ["PythonAnywherePublicationChannel"]
